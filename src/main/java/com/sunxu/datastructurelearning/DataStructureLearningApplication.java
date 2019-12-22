db8d8b6406328ee60b475c6f927ab19ff2d53cec
孙许 <sunxu@sunxudeMacBook-Pro.local>
2019-12-22 22:24:07 +0800

    Initial commit

---
+++ src/main/java/com/sunxu/datastructurelearning/DataStructureLearningApplication.java
@@ -0,0 +1,13 @@
+package com.sunxu.datastructurelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunxu/datastructurelearning/DataStructureLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunxu.datastructurelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
